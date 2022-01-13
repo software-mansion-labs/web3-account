68bb461a9973b90a3a3cbc6cea36d08f011ade22
Jakub Ptak <jakub.ptak@swmansion.com>
2022-01-13 16:58:22 +0100

    Add signing custom payloads

--- client/app.tsx
+++ client/app.tsx
@@ -1,15 +1,70 @@
 import ReactDOM from "react-dom";
-import {useEffect} from "react";
-import {getLib} from "./lib";
+import {useMemo, useState} from "react";
+import {getLib, Lib} from "./lib";
+
+const padded = {display: "block", padding: 20};
 
 const App = () => {
-    useEffect(() => {
-        getLib().then(l => l.suggestChain())
-    }, [])
-    return <div>
+    const [lib, setLib] = useState<Lib>();
+    const [address, setAddress] = useState("0x03606DB92E563E41F4A590BC01C243E8178E9BA8C980F8E464579F862DA3537C");
+    const [selector, setSelector] = useState("1530486729947006463063166157847785599120665941190480211966374137237989315360");
+    const [calldata, setCalldata] = useState("1234, 11")
+    const [signature, setSignature] = useState("");
 
+    const payload = useMemo(() => {
+        try {
+            return ({
+                address: BigInt(address),
+                selector: BigInt(selector),
+                calldata: calldata.split(",").map(v => v.trim()).filter(v => v).map(BigInt),
+            });
+        } catch (e) {
+            return undefined
+        }
+    }, [address, selector, calldata]);
+
+    return <div>
+        <div style={padded}>
+            <button disabled={!!lib} onClick={async () => {
+                const lib = await getLib();
+                await lib.useStarknet()
+                await lib.connectAccounts();
+                setLib(lib);
+            }}>
+                CONNECT TO METAMASK
+            </button>
+        </div>
+        <div style={padded}>
+            <label style={padded}>
+                Address
+                <input style={{display: "block"}} onChange={e => setAddress(e.target.value)} value={address}/>
+            </label>
+            <label style={padded}>
+                Selector
+                <input style={{display: "block"}} onChange={e => setSelector(e.target.value)} value={selector}/>
+            </label>
+            <label style={padded}>
+                Calldata
+                <input style={{display: "block"}} onChange={e => setCalldata(e.target.value)} value={calldata}/>
+            </label>
+        </div>
+        <div style={padded}>
+            {payload && <pre>PAYLOAD {JSON.stringify({
+                address: payload.address.toString(),
+                selector: payload.selector.toString(),
+                calldata: payload.calldata.toString(),
+            }, null, 2)}</pre>}
+        </div>
+        <div style={padded}>
+            <button disabled={!lib || !payload} onClick={() => lib.sign(payload).then(setSignature)}>
+                GENERATE SIGNATURE
+            </button>
+        </div>
+        <div style={padded}>
+            {signature && <pre>{signature}</pre>}
+        </div>
     </div>;
 };
 
 const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+ReactDOM.render(<App/>, app);
--- client/lib.ts
+++ client/lib.ts
@@ -1,6 +1,5 @@
 import detectEthereumProvider = require("@metamask/detect-provider");
-import { MetaMaskInpageProvider } from "@metamask/providers";
-
+import {MetaMaskInpageProvider} from "@metamask/providers";
 
 const chain = {
     chainId: "0xb",
@@ -8,10 +7,84 @@ const chain = {
     rpcUrls: ["https://localhost:8000"]
 }
 
-export class Lib {
-    constructor(private provider: MetaMaskInpageProvider) {}
+const chainId = "0xb";
 
-    suggestChain = () => this.provider.request({method: "wallet_addEthereumChain", params: [chain]})
+const makeData = (payload: Payload): string => JSON.stringify({
+    domain: {
+        chainId: chainId,
+        name: 'Starknet adapter',
+        version: '1',
+    },
+
+    // Defining the message signing data content.
+    message: {
+        address: payload.address.toString(),
+        selector: payload.selector.toString(),
+        calldata: payload.calldata.map(v => v.toString())
+    },
+    // Refers to the keys of the *types* object below.
+    primaryType: 'Payload',
+    types: {
+        EIP712Domain: [
+            {name: 'name', type: 'string'},
+            {name: 'version', type: 'string'},
+            {name: 'chainId', type: 'uint256'},
+        ],
+        Payload: [
+            {name: 'address', type: 'uint256'},
+            {name: 'selector', type: 'uint256'},
+            {name: 'calldata', type: 'uint256[]'},
+        ],
+    },
+});
+
+interface Payload {
+    address: bigint;
+    selector: bigint;
+    calldata: bigint[];
+}
+
+class MetamaskClient {
+    constructor(protected provider: MetaMaskInpageProvider) {
+    }
+
+    request = (method: string, ...params: any[]) => this.provider.request({method, params})
+}
+
+const UNRECOGNIZED_CHAIN = 4902;
+
+export class Lib extends MetamaskClient {
+    private accounts?: string[] = undefined;
+
+    useStarknet = async () => {
+        try {
+            await this.switch();
+        } catch (e) {
+            if ("code" in e && e.code === UNRECOGNIZED_CHAIN) {
+                await this.request("wallet_addEthereumChain", chain);
+                await this.switch();
+                return
+            }
+            throw e;
+        }
+    }
+
+    connectAccounts = async () => {
+        await this.request("eth_requestAccounts")
+        const accounts = await this.request("eth_accounts") as string[];
+        this.accounts = [...accounts];
+        return accounts
+    }
+
+    private switch = () => this.request("wallet_switchEthereumChain", {chainId});
+
+    sign = async (payload: Payload) => {
+        if (!this.accounts || !this.accounts[0]) {
+            throw new Error("No account available");
+        }
+        console.log("DATA", makeData(payload))
+        return await this.request("eth_signTypedData_v4", this.accounts[0], makeData(payload));
+    }
 }
 
 export const getLib = async (): Promise<Lib | undefined> => {
